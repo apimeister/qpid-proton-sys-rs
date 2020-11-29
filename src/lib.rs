60edbe8fad207c6568bac74eadd37d98c56aa75f
Jens Walter <jens@apimeister.com>
2020-11-29 19:31:52 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -2,71 +2,139 @@ use std::os::raw::c_char;
 
 #[allow(dead_code)]
 extern "C" {
-  pub fn pn_proactor() -> *mut pn_proactor_t;
-  pub fn pn_proactor_free(proactor: *mut pn_proactor_t);
-  pub fn pn_message()	-> *mut pn_message_t;
-  pub fn pn_message_free(message: *mut pn_message_t);
-  pub fn pn_proactor_addr(addr: *mut c_char, size: usize, host: *const c_char, port: *const c_char ) -> i64;
-  pub fn pn_proactor_connect2	(proactor: *mut pn_proactor_t,connection: *mut pn_connection_t,
-    transport: *mut pn_transport_t,addr: *const c_char);
-  pub fn pn_proactor_wait(proactor: *mut pn_proactor_t) -> *mut pn_event_batch_t;
-  pub fn pn_event_batch_next(batch: *mut pn_event_batch_t)	-> *mut pn_event_t;
-  pub fn pn_proactor_done(proactor: *mut pn_proactor_t, events: *mut pn_event_batch_t);
-  pub fn pn_event_type(event: *mut pn_event_t) -> pn_event_type_t;
-  pub fn pn_event_connection(event: *mut pn_event_t) -> *mut pn_connection_t;
-  pub fn pn_event_session(event: *mut pn_event_t) -> *mut pn_session_t;
-  pub fn pn_connection() -> *mut pn_connection_t;
-  pub fn pn_connection_set_container(connection: *mut pn_connection_t, container: *const c_char);
-  pub fn pn_connection_open(connection: *mut pn_connection_t);
-  pub fn pn_connection_close(connection: *mut pn_connection_t);
-  pub fn pn_session_open(session: *mut pn_session_t);
-  pub fn pn_session(connection: *mut pn_connection_t) -> *mut pn_session_t;
-  pub fn pn_sender(session: *mut pn_session_t, name: *const c_char) -> *mut pn_link_t;
-  pub fn pn_terminus_set_address(terminus: *mut pn_terminus_t, address: *const c_char) -> i64;
-  pub fn pn_link_open(link: *mut pn_link_t);	
-  pub fn pn_link_target(link: *mut pn_link_t) -> *mut pn_terminus_t;
-  pub fn pn_transport_condition(transport: *mut pn_transport_t) -> *mut pn_condition_t;
+  pub fn pn_bytes(size: usize,start: *const c_char) -> *mut pn_bytes_t;
+
   pub fn pn_condition_get_name(condition: *mut pn_condition_t) -> *const c_char;
   pub fn pn_condition_get_description(condition: *mut pn_condition_t) -> *const c_char;
-  pub fn pn_transport() -> *mut pn_transport_t;
-  pub fn pn_transport_require_auth(transport: *mut pn_transport_t,required: bool);
-  pub fn pn_transport_require_encryption(transport: *mut pn_transport_t,required: bool);
+
+  /// Factory to construct a new Connection.
+  pub fn pn_connection() -> *mut pn_connection_t;
+  /// Close a connection.
+  /// Once this operation has completed, the PN_LOCAL_CLOSED state flag will be set. This may be called without calling pn_connection_open, in this case it is equivalent to calling pn_connection_open followed by pn_connection_close.
+  pub fn pn_connection_close(connection: *mut pn_connection_t);
+  /// Open a connection.
+  /// Once this operation has completed, the PN_LOCAL_ACTIVE state flag will be set.
+  pub fn pn_connection_open(connection: *mut pn_connection_t);
+  /// Set the AMQP Container name advertised by a connection object.
+  pub fn pn_connection_set_container(connection: *mut pn_connection_t, container: *const c_char);
   pub fn pn_connection_set_hostname(connection: *mut pn_connection_t, hostname: *const c_char);
-  pub fn pn_connection_set_user(connection: *mut pn_connection_t,user: *const c_char);
   pub fn pn_connection_set_password(connection: *mut pn_connection_t,password: *const c_char);
-  pub fn pn_sasl(transport: *mut pn_transport_t) -> *mut pn_sasl_t;
-  pub fn pn_sasl_set_allow_insecure_mechs(sasl: *mut pn_sasl_t,insecure: bool);
-  pub fn pn_event_link(event: *mut pn_event_t) -> *mut pn_link_t;
-  pub fn pn_link_credit(link: *mut pn_link_t) -> i64;
-  pub fn pn_dtag(bytes: *const c_char,size: usize) -> pn_delivery_tag_t;
-  pub fn pn_delivery(link: *mut pn_link_t,tag: pn_delivery_tag_t) -> *mut pn_delivery_t;
-  pub fn pn_message_clear(msg: *mut pn_message_t);
-  pub fn pn_message_body(msg: *mut pn_message_t) -> *mut pn_data_t;
+  pub fn pn_connection_set_user(connection: *mut pn_connection_t,user: *const c_char);
+
   pub fn pn_data_put_int(data: *mut pn_data_t,i: i32) -> i64;
-  pub fn pn_message_id(msg: *mut pn_message_t) -> *mut pn_data_t;
   pub fn pn_data_enter(data: *mut pn_data_t) -> bool;
   pub fn pn_data_put_map(data: *mut pn_data_t) -> i64;
   pub fn pn_data_put_string(data: *mut pn_data_t,string: *mut pn_bytes_t) -> i64;
   pub fn pn_data_exit(data: *mut pn_data_t) -> bool;
-  pub fn pn_error_set(error: *mut pn_error_t,code: i64,text: *const c_char) -> i64;
-  pub fn pn_message_error(msg: *mut pn_message_t) -> *mut pn_error_t;
-  pub fn pn_error_text(error: *mut pn_error_t) -> *const c_char;
-  pub fn pn_bytes(size: usize,start: *const c_char) -> *mut pn_bytes_t;
-  pub fn pn_message_send(msg: *mut pn_message_t,sender: *mut pn_link_t,buf: *mut pn_rwbytes_t) -> i64;
-  pub fn pn_event_delivery(event: *mut pn_event_t) -> *mut pn_delivery_t;
+
+  pub fn pn_delivery(link: *mut pn_link_t,tag: pn_delivery_tag_t) -> *mut pn_delivery_t;
   pub fn pn_delivery_remote_state(delivery: *mut pn_delivery_t) -> u64;
+
+  pub fn pn_dtag(bytes: *const c_char,size: usize) -> pn_delivery_tag_t;
+
+  pub fn pn_error_set(error: *mut pn_error_t,code: i64,text: *const c_char) -> i64;
+  pub fn pn_error_text(error: *mut pn_error_t) -> *const c_char;
+
+  pub fn pn_event_batch_next(batch: *mut pn_event_batch_t)	-> *mut pn_event_t;
+  pub fn pn_event_connection(event: *mut pn_event_t) -> *mut pn_connection_t;
+  pub fn pn_event_delivery(event: *mut pn_event_t) -> *mut pn_delivery_t;
+
+  pub fn pn_event_link(event: *mut pn_event_t) -> *mut pn_link_t;
+  pub fn pn_event_session(event: *mut pn_event_t) -> *mut pn_session_t;
+  pub fn pn_event_type(event: *mut pn_event_t) -> pn_event_type_t;
+
+
+  /// Advance the current delivery of a link to the next delivery on the link.
+  /// For sending links this operation is used to finish sending message data for the current outgoing delivery and move on to the next outgoing delivery (if any).
+  /// For receiving links, this operation is used to finish accessing message data from the current incoming delivery and move on to the next incoming delivery (if any).
+  /// Each link maintains a sequence of deliveries in the order they were created, along with a pointer to the current delivery. The pn_link_advance operation will modify the current delivery on the link to point to the next delivery in the sequence. If there is no next delivery in the sequence, the current delivery will be set to NULL. This operation will return true if invoking it caused the value of the current delivery to change, even if it was set to NULL.
+  pub fn pn_link_advance(link: *mut pn_link_t) -> bool;
+  /// Get the credit balance for a link.
+  /// Links use a credit based flow control scheme. Every receiver maintains a credit balance that corresponds to the number of deliveries that the receiver can accept at any given moment. As more capacity becomes available at the receiver (see pn_link_flow), it adds credit to this balance and communicates the new balance to the sender. Whenever a delivery is sent/received, the credit balance maintained by the link is decremented by one. Once the credit balance at the sender reaches zero, the sender must pause sending until more credit is obtained from the receiver.
+  /// Note that a sending link may still be used to send deliveries even if pn_link_credit reaches zero, however those deliveries will end up being buffered by the link until enough credit is obtained from the receiver to send them over the wire. In this case the balance reported by pn_link_credit will go negative.
+  pub fn pn_link_credit(link: *mut pn_link_t) -> i64;
+  /// Grant credit for incoming deliveries on a receiver.
+  pub fn pn_link_flow(receiver: *mut pn_link_t, credit: i32);
+  /// Open a link.
+  /// Once this operation has completed, the PN_LOCAL_ACTIVE state flag will be set.
+  pub fn pn_link_open(link: *mut pn_link_t);
+  /// Send message data for the current delivery on a link.
+  pub fn pn_link_send(sender: *mut pn_link_t,bytes: *const c_char,n: usize) -> usize;
+  /// Access the locally defined target definition for a link.
+  /// The pointer returned by this operation is valid until the link object is freed.
+  pub fn pn_link_target(link: *mut pn_link_t) -> *mut pn_terminus_t;
+  pub fn pn_logger_set_mask(logger: *mut pn_logger_t,subsystem: pn_log_subsystem_t, level: pn_log_level_t);
+
+  /// Construct a new pn_message_t.
+  /// Every message that is constructed must be freed using pn_message_free().
+  pub fn pn_message()	-> *mut pn_message_t;
+  /// Get and set the body of a message.
+  /// This operation returns a pointer to a pn_data_t representing the body of a message. The pointer is valid until the message is freed and may be used to both access and modify the content of the message body.
+  pub fn pn_message_body(msg: *mut pn_message_t) -> *mut pn_data_t;
+  /// Clears the content of a pn_message_t.
+  /// When pn_message_clear returns, the supplied pn_message_t will be emptied of all content and effectively returned to the same state as if it was just created.
+  pub fn pn_message_clear(msg: *mut pn_message_t);
+  /// Encode a message as AMQP formatted binary data.
+  /// If the buffer space provided is insufficient to store the content held in the message, the operation will fail and return a PN_OVERFLOW error code.
+  pub fn pn_message_encode(msg: *mut pn_message_t,bytes: *const c_char,size: *mut usize) -> i64;
+  /// Free a previously constructed pn_message_t.
+  pub fn pn_message_error(msg: *mut pn_message_t) -> *mut pn_error_t;
+  /// Free a previously constructed pn_message_t.
+  pub fn pn_message_free(message: *mut pn_message_t);
+  /// Get/set the id for a message.
+  /// The message id provides a globally unique identifier for a message. A message id can be an a string, an unsigned long, a uuid or a binary value. This operation returns a pointer to a pn_data_t that can be used to access and/or modify the value of the message id. The pointer is valid until the message is freed. See pn_data_t for details on how to get/set the value.
+  pub fn pn_message_id(msg: *mut pn_message_t) -> *mut pn_data_t;
+  /// Encode and send a message on a sender link.
+  /// Performs the following steps:
+  /// call pn_message_encode2() to encode the message to a buffer
+  /// call pn_link_send() to send the encoded message bytes
+  /// call pn_link_advance() to indicate the message is complete
+  /// Note: you must create a delivery for the message before calling pn_message_send() see pn_delivery()
+  pub fn pn_message_send(msg: *mut pn_message_t,sender: *mut pn_link_t,buf: *mut pn_rwbytes_t) -> i64;
+  /// Create a proactor.
+  /// Must be freed with pn_proactor_free()
+  pub fn pn_proactor() -> *mut pn_proactor_t;
+  pub fn pn_proactor_addr(addr: *mut c_char, size: usize, host: *const c_char, port: *const c_char ) -> i64;
+  /// Free the proactor.
+  /// Abort open connections/listeners, clean up all resources.
+  pub fn pn_proactor_connect2	(proactor: *mut pn_proactor_t,connection: *mut pn_connection_t,
+    transport: *mut pn_transport_t,addr: *const c_char);
+  pub fn pn_proactor_done(proactor: *mut pn_proactor_t, events: *mut pn_event_batch_t);
+  pub fn pn_proactor_free(proactor: *mut pn_proactor_t);
+  pub fn pn_proactor_wait(proactor: *mut pn_proactor_t) -> *mut pn_event_batch_t;
+
+  pub fn pn_sasl(transport: *mut pn_transport_t) -> *mut pn_sasl_t;
+  pub fn pn_sasl_allowed_mechs(sasl: *mut pn_sasl_t,mechs: *const c_char);
+  pub fn pn_sasl_set_allow_insecure_mechs(sasl: *mut pn_sasl_t,insecure: bool);
+
+  /// Factory for creating a new session on a given connection object.
+  /// Creates a new session object and adds it to the set of sessions maintained by the connection object.
+  pub fn pn_session(connection: *mut pn_connection_t) -> *mut pn_session_t;
+  /// Close a session.
+  /// Once this operation has completed, the PN_LOCAL_CLOSED state flag will be set. This may be called without calling pn_session_open, in this case it is equivalent to calling pn_session_open followed by pn_session_close.
+  pub fn pn_session_close(session: *mut pn_session_t);
+  /// Free a session object.
+  /// When a session is freed it will no longer be retained by the connection once any internal references to the session are no longer needed. Freeing a session will free all links on that session and settle any deliveries on those links.
+  pub fn pn_session_free(session: *mut pn_session_t);
+  /// Open a session.
+  /// Once this operation has completed, the PN_LOCAL_ACTIVE state flag will be set.
+  pub fn pn_session_open(session: *mut pn_session_t);
+  pub fn pn_session_remote_condition(session: *mut pn_session_t) -> *mut pn_condition_t;
+
+  pub fn pn_sender(session: *mut pn_session_t, name: *const c_char) -> *mut pn_link_t;
   pub fn pn_ssl(transport: *mut pn_transport_t) -> *mut pn_ssl_t;
   pub fn pn_ssl_domain(mode: pn_ssl_mode_t) -> *mut pn_ssl_domain_t;
-  pub fn pn_ssl_init(ssl: *mut pn_ssl_t,domain: *mut pn_ssl_domain_t,session_id: *const c_char) -> i64;
-  pub fn pn_transport_logger(transport: *mut pn_transport_t) -> *mut pn_logger_t;
-  pub fn pn_logger_set_mask(logger: *mut pn_logger_t,subsystem: pn_log_subsystem_t, level: pn_log_level_t);
-  pub fn pn_sasl_allowed_mechs(sasl: *mut pn_sasl_t,mechs: *const c_char);
-  pub fn pn_ssl_set_peer_hostname(ssl: *mut pn_ssl_t,hostname: *const c_char) -> i64;
-  pub fn pn_link_advance(link: *mut pn_link_t) -> bool;
-  pub fn pn_session_remote_condition(session: *mut pn_session_t) -> *mut pn_condition_t;
   pub fn pn_ssl_get_remote_subject(ssl: *mut pn_ssl_t) -> *const c_char;
-  pub fn pn_link_send(sender: *mut pn_link_t,bytes: *const c_char,n: usize) -> usize;
-  pub fn pn_message_encode(msg: *mut pn_message_t,bytes: *const c_char,size: *mut usize) -> i64;
+  pub fn pn_ssl_init(ssl: *mut pn_ssl_t,domain: *mut pn_ssl_domain_t,session_id: *const c_char) -> i64;
+  pub fn pn_ssl_set_peer_hostname(ssl: *mut pn_ssl_t,hostname: *const c_char) -> i64;
+
+  pub fn pn_terminus_set_address(terminus: *mut pn_terminus_t, address: *const c_char) -> i64;
+
+  pub fn pn_transport_condition(transport: *mut pn_transport_t) -> *mut pn_condition_t;
+  pub fn pn_transport() -> *mut pn_transport_t;
+  pub fn pn_transport_require_auth(transport: *mut pn_transport_t,required: bool);
+  pub fn pn_transport_require_encryption(transport: *mut pn_transport_t,required: bool);
+  pub fn pn_transport_logger(transport: *mut pn_transport_t) -> *mut pn_logger_t;
 }
 
 
